c779443b44a49dd14b98e76a26ec3d7f72cc08a0
hatim hatimi <hhatimi@dept-info.crosemont.quebec>
2024-11-05 11:58:40 -0500

    population favoris et ajout image

--- app/src/main/java/com/example/panocartemtl/fragment_favoris.kt
+++ app/src/main/java/com/example/panocartemtl/fragment_favoris.kt
@@ -1,22 +1,39 @@
 package com.example.panocartemtl
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
 
 class fragment_favoris : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favoris, container, false)
+        val view = inflater.inflate(R.layout.fragment_favoris, container, false)
+
+        // Initialisation de la ListView
+        val listView: ListView = view.findViewById(R.id.listViewFavoris)
+
+        // Adresses fictives
+        val adresses = listOf(
+            "1234 Rue Imaginaire, Montréal, QC H1A 1A1",
+            "5678 Avenue Fictive, Montréal, QC H2B 2B2",
+            "91011 Boulevard Faux, Montréal, QC H3C 3C3",
+            "1213 Chemin Illusoire, Montréal, QC H4D 4D4",
+            "1415 Route Mensongère, Montréal, QC H5E 5E5"
+        )
+
+        // Création de l'adaptateur
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item_favoris, R.id.txtAdresse, adresses)
+
+        // Assignation de l'adaptateur à la ListView
+        listView.adapter = adapter
+
+        return view
     }
 }
