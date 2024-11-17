a0754d5e1b2e7f92a063cd094d3bdf9888ff364a
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-11-16 19:39:46 -0500

    Version complète vu en Sprint 2 qui utilisait Google Maps en Mapbox

--- app/src/main/java/com/example/panocartemtl/fragment_carte.kt
+++ app/src/main/java/com/example/panocartemtl/fragment_carte.kt
@@ -1,5 +1,6 @@
 package com.example.panocartemtl
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,10 +15,15 @@ import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.annotation.AnnotationPlugin
-//import com.mapbox.maps.plugin.annotation.generated.Marker
-//import com.mapbox.maps.plugin.annotation.generated.MarkerOptions
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.geojson.Point
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
 import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.OnPointAnnotationClickListener
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 
 class fragment_carte : Fragment() {
     private lateinit var popupLayout: View
@@ -27,6 +33,9 @@ class fragment_carte : Fragment() {
     private lateinit var navController: NavController
     private lateinit var mapView: MapView
     private lateinit var annotationPlugin: AnnotationPlugin
+    private lateinit var pointAnnotationManager: PointAnnotationManager
+
+    private val markerMap: MutableMap<PointAnnotation, String> = mutableMapOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,8 +52,11 @@ class fragment_carte : Fragment() {
         mapView = view.findViewById(R.id.map)
         mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { style ->
             annotationPlugin = mapView.annotations
+            pointAnnotationManager = annotationPlugin.createPointAnnotationManager() // Correctly initialize PointAnnotationManager
             setupMap(style)
         }
+
+        navController = findNavController()
 
         // Instancier les composantes
         popupLayout = view.findViewById(R.id.popupLayout)
@@ -52,7 +64,7 @@ class fragment_carte : Fragment() {
         btnPostion = view.findViewById(R.id.btnPositionActuelle)
         btnFavoris = view.findViewById(R.id.btnFavoris)
 
-        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.menu_navigation)
+        val menuView = requireActivity().findViewById<BottomNavigationView>(R.id.menu_navigation)
 
         // Cacher popup
         popupBouton.setOnClickListener {
@@ -60,14 +72,10 @@ class fragment_carte : Fragment() {
         }
 
         // Navigation menu
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+        menuView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.navigation_carte -> {
-                    true
-                }
-                R.id.navigation_recherche -> {
-                    true
-                }
+                R.id.navigation_carte -> true
+                R.id.navigation_recherche -> true
                 R.id.navigation_favoris -> {
                     navController.navigate(R.id.action_fragment_carte_vers_fragment_favoris)
                     true
@@ -78,42 +86,64 @@ class fragment_carte : Fragment() {
     }
 
     private fun setupMap(style: Style) {
-        /*
-        // Define coordinates
-        val coordRosemont = com.mapbox.geojson.Point.fromLngLat(-73.5826029, 45.5571566)
-        val coordInsectarium = com.mapbox.geojson.Point.fromLngLat(-73.554640, 45.561120)
 
-        // Add markers to the map
-        //val markerRosemont = annotationPlugin.createMarker(MarkerOptions().withLatLng(coordRosemont).withTitle("Position de Rosemont"))
-        //val markerInsectarium = annotationPlugin.createMarker(MarkerOptions().withLatLng(coordInsectarium).withTitle("Position de l'Insectarium"))
+        style.addImage("marqueur_rouge", BitmapFactory.decodeResource(resources, R.drawable.marqueur_rouge))
 
-        // Set initial camera position
+        // Rosemont
+        val coordRosemont = Point.fromLngLat(-73.5826029, 45.5571566)
+
+        val rosemont = PointAnnotationOptions()
+            .withPoint(coordRosemont)
+            .withIconImage("marqueur_rouge")
+            .withIconAnchor(IconAnchor.BOTTOM)
+            .withIconSize(0.6)
+
+        val rosemontAnnotation = pointAnnotationManager.create(rosemont)
+        markerMap[rosemontAnnotation] = "rosemont"
+
+        // Insectarium
+        val coordInsectarium = Point.fromLngLat(-73.554640, 45.561120)
+
+        val insectarium = PointAnnotationOptions()
+            .withPoint(coordInsectarium)
+            .withIconImage("marqueur_rouge")
+            .withIconAnchor(IconAnchor.BOTTOM)
+            .withIconSize(0.6)
+
+        val insectariumAnnotation = pointAnnotationManager.create(insectarium)
+        markerMap[insectariumAnnotation] = "insectarium"
+
+        // Première vue
         mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(coordInsectarium).zoom(13.0).build())
 
-        // Marker click event
-        annotationPlugin.addClickListener { marker ->
-            if (marker == markerRosemont) {
-                Toast.makeText(requireContext(), "Marqueur cliqué: ${marker.title}", Toast.LENGTH_SHORT).show()
-                montrerPopup("Position de Rosemont", "6400 16e Avenue, Montréal, QC H1X 2S9")
-            } else if (marker == markerInsectarium) {
-                Toast.makeText(requireContext(), "Marqueur cliqué: ${marker.title}", Toast.LENGTH_SHORT).show()
-                montrerPopup("Position de l'Insectarium", "4581 Sherbrooke St E, Montreal, QC H1X 2B2")
-            }
-            true
-        }
-
-        // Event for position button
+        // Boutons dans la vue carte
         btnPostion.setOnClickListener {
             mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(coordRosemont).zoom(18.0).build())
         }
 
-        // Event for favoris button
         btnFavoris.setOnClickListener {
             navController.navigate(R.id.action_fragment_carte_vers_fragment_favoris)
         }
 
-         */
+        // Cliquer sur les marqueurs
+        pointAnnotationManager.addClickListener(object : OnPointAnnotationClickListener {
+            override fun onAnnotationClick(pointAnnotation: PointAnnotation): Boolean {
+                val marqueurId = markerMap[pointAnnotation] // Get the marker ID from the map
+                when (marqueurId) {
+                    "rosemont" -> {
+                        Toast.makeText(requireContext(), "Marqueur cliqué: Position de Rosemont", Toast.LENGTH_SHORT).show()
+                        montrerPopup("Position de Rosemont", "6400 16e Avenue, Montréal, QC H1X 2S9")
+                    }
+                    "insectarium" -> {
+                        Toast.makeText(requireContext(), "Marqueur cliqué: Position de l'Insectarium", Toast.LENGTH_SHORT).show()
+                        montrerPopup("Position de l'Insectarium", "4581 Sherbrooke St E, Montreal, QC H1X 2B2")
+                    }
+                }
+                return true
+            }
+        })
     }
+
 
     private fun montrerPopup(titre: String, description: String) {
         popupLayout.findViewById<TextView>(R.id.txtAdresse).text = description
